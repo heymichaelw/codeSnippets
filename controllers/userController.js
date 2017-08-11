6fbc53957f85e8f18871b2b0d59df084c2541354
Michael <heymichaelw@gmail.com>
2017-08-11 13:40:14 -0400

    user model tests passing

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,5 +6,32 @@ mongoose.Promise = require('bluebird');
 module.exports = {
   createUser: function(username, password){
       return User.create({username: username, password: helpers.createPasswordHashObject(password)});
-    }
+    },
+  login: function(username, password){
+    return User.findOne({username: username}).then(function(user){
+  if(!user){
+    return false;
+  }
+  const pwObject = user.password;
+  const newPwObject = helpers.createPasswordHashObject(password, pwObject.salt);
+  return pwObject.hash === newPwObject.hash;
+    });
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 };
--- tests/tests.js
+++ tests/tests.js
@@ -6,6 +6,13 @@ const Snippet = require('../models/snippet');
 const userController = require('../controllers/userController.js');
 
 describe("user model tests", function(){
+
+  afterEach(function(done){
+    User.deleteMany({}).then(function(){
+      done();
+    });
+  });
+
   it("can create a user", function(done){
     userController.createUser("username", "password").then(function(user){
       expect(user.username).to.equal("username");
@@ -14,7 +21,37 @@ describe("user model tests", function(){
     });
     done();
   });
+
+  it('can log in and return true if valid login', function(done){
+    userController.createUser("michael", "fredmoo").then(function(user){
+      userController.login("michael", "fredmoo").then(function(result){
+        expect(result).to.equal(true);
+        done();
+      });
+    });
+  });
+
+  it('will not login if invalid password', function(done){
+    userController.createUser("michael", "fredmoo").then(function(user){
+      userController.login("michael", "kaminsky").then(function(result){
+        expect(result).to.equal(false);
+        done();
+      });
+    });
+  });
+
+  it('will not login if invalid username', function(done){
+    userController.createUser("michael", "fredmoo").then(function(user){
+      userController.login("tommy", "fredmoo").then(function(result){
+        expect(result).to.equal(false);
+        done();
+      });
+    });
+  });
+
 });
+
+
 
 describe("sanity test", function(){
    it("should run test", function(){
